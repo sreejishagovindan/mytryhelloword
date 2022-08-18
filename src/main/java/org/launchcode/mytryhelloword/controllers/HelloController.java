0c2dfed17e6998fd1f56f3ee9bbc6da0d6e206e7
sreejishagovindan <100387980+sreejishagovindan@users.noreply.github.com>
2022-08-18 18:28:22 -0500

    made changes using thymeleaf static template

--- src/main/java/org/launchcode/mytryhelloword/controllers/HelloController.java
+++ src/main/java/org/launchcode/mytryhelloword/controllers/HelloController.java
@@ -6,22 +6,25 @@ import org.springframework.web.bind.annotation.*;
 import javax.websocket.server.PathParam;
 
 @Controller
-@ResponseBody
-@RequestMapping("hello")
+
+
 public class HelloController {
 
     @GetMapping()
+    @ResponseBody
     public String hello() {
         return "Hello, spring";
     }
 // it lives at /hello/goodbye as we have requestmapping("hello") above the class
     @GetMapping("goodbye")
+    @ResponseBody
     public String bye() {
         return "Goodbye, spring";
     }
     // handles request of the form /hello?name=jisha
 
-   @RequestMapping(method={RequestMethod.GET,RequestMethod.POST})
+   @RequestMapping(method={RequestMethod.GET,RequestMethod.POST},value="hello")
+   @ResponseBody
 
     public String helloParam(@RequestParam String name){
         return "Hello "+ name +" !!!";
@@ -35,14 +38,7 @@ public class HelloController {
     @GetMapping("form")
 // it lives at /hello/form as we have requestmapping("hello")  annotation above the class
     public String helloForm(){
-        return "<html>" +
-                "<body>" +
-                "<form action='hello' method='post'>" +//submit the request to /hello
-                "<input type='text' name='name'>" +
-                "<input type='submit' value='Greet Me'>" +
-                "</form>" +
-                "</body>" +
-                "<html>";
+        return "form";
     }
 
 }
