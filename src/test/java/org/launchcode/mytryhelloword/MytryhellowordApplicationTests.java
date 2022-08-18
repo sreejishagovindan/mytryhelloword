84c6156298beaf9f0e9d17a398629b5599c65e95
sreejishagovindan <100387980+sreejishagovindan@users.noreply.github.com>
2022-08-18 10:10:52 -0500

    added spring frame work

---
+++ src/test/java/org/launchcode/mytryhelloword/MytryhellowordApplicationTests.java
@@ -0,0 +1,13 @@
+package org.launchcode.mytryhelloword;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MytryhellowordApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
