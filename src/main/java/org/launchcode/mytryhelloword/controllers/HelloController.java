bce9627e6ff6ef78076b061d9f8ade2034ec9881
sreejishagovindan <100387980+sreejishagovindan@users.noreply.github.com>
2022-08-18 12:25:21 -0500

    changes made took annotation above the class

--- src/main/java/org/launchcode/mytryhelloword/controllers/HelloController.java
+++ src/main/java/org/launchcode/mytryhelloword/controllers/HelloController.java
@@ -6,31 +6,34 @@ import org.springframework.web.bind.annotation.*;
 import javax.websocket.server.PathParam;
 
 @Controller
+@ResponseBody
+@RequestMapping("hello")
 public class HelloController {
-    @ResponseBody
+
     @GetMapping()
     public String hello() {
         return "Hello, spring";
     }
-    @ResponseBody
+// it lives at /hello/goodbye as we have requestmapping("hello") above the class
     @GetMapping("goodbye")
     public String bye() {
         return "Goodbye, spring";
     }
     // handles request of the form /hello?name=jisha
-   @RequestMapping(method={RequestMethod.GET,RequestMethod.POST},value="hello")
-    @ResponseBody
+
+   @RequestMapping(method={RequestMethod.GET,RequestMethod.POST})
+
     public String helloParam(@RequestParam String name){
         return "Hello "+ name +" !!!";
     }
     // handles request of the form /hello/jisha
-   // @GetMapping("hello/{name}")
-   // @ResponseBody
-    //public String helloWithPathParam(@PathVariable String name){
-        //return "Hello " +name+" !!!";
-    //}
+    @GetMapping("{name}")
+   @ResponseBody
+    public String helloWithPathParam(@PathVariable String name){
+        return "Hello " +name+" !!!";
+    }
     @GetMapping("form")
-    @ResponseBody
+// it lives at /hello/form as we have requestmapping("hello")  annotation above the class
     public String helloForm(){
         return "<html>" +
                 "<body>" +
