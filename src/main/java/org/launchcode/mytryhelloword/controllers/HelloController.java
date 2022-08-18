e6768e272b9e3687869dc482c2a90e09f0ca8269
sreejishagovindan <100387980+sreejishagovindan@users.noreply.github.com>
2022-08-18 11:35:41 -0500

    changes made in controller using requestparam anotation

--- src/main/java/org/launchcode/mytryhelloword/controllers/HelloController.java
+++ src/main/java/org/launchcode/mytryhelloword/controllers/HelloController.java
@@ -2,12 +2,16 @@ package org.launchcode.mytryhelloword.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.websocket.server.PathParam;
 
 @Controller
 public class HelloController {
     @ResponseBody
-    @GetMapping("hello")
+    @GetMapping()
     public String hello() {
         return "Hello, spring";
     }
@@ -16,4 +20,17 @@ public class HelloController {
     public String bye() {
         return "Goodbye, spring";
     }
+    // handles request of the form /hello?name=jisha
+    //@GetMapping("hello")
+    //@ResponseBody
+    //public String helloParam(@RequestParam String name){
+        //return "Hello "+ name +" !!!";
+    //}
+    // handles request of the form /hello/jisha
+    @GetMapping("hello/{name}")
+    @ResponseBody
+    public String helloWithPathParam(@PathVariable String name){
+        return "Hello " +name+" !!!";
+    }
+
 }
