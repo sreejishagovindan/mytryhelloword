c34fd93ed5ac269e3efd77a9bd5bb5983b545272
sreejishagovindan <100387980+sreejishagovindan@users.noreply.github.com>
2022-08-18 19:56:38 -0500

    made changes using loop

--- src/main/java/org/launchcode/mytryhelloword/controllers/HelloController.java
+++ src/main/java/org/launchcode/mytryhelloword/controllers/HelloController.java
@@ -1,9 +1,12 @@
 package org.launchcode.mytryhelloword.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import javax.websocket.server.PathParam;
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 
@@ -24,21 +27,34 @@ public class HelloController {
     // handles request of the form /hello?name=jisha
 
    @RequestMapping(method={RequestMethod.GET,RequestMethod.POST},value="hello")
-   @ResponseBody
 
-    public String helloParam(@RequestParam String name){
-        return "Hello "+ name +" !!!";
+
+    public String helloParam(@RequestParam String name, Model model){
+       String greeting="hello " + name + " !!!";
+        model.addAttribute("greeting",greeting);
+        return "hello";
     }
     // handles request of the form /hello/jisha
     @GetMapping("{name}")
-   @ResponseBody
-    public String helloWithPathParam(@PathVariable String name){
-        return "Hello " +name+" !!!";
+
+    public String helloWithPathParam(@PathVariable String name,Model model){
+        String greeting="hello " + name + " !!!";
+        model.addAttribute("greeting",greeting);
+        return "hello";
     }
     @GetMapping("form")
 // it lives at /hello/form as we have requestmapping("hello")  annotation above the class
     public String helloForm(){
         return "form";
+    }
+    @GetMapping("hello-names")
+    public String helloNames(Model model){
+        List<String> names=new ArrayList<>();
+        names.add("Launch Code");
+        names.add("Java");
+        names.add("Java Script");
+        model.addAttribute("names",names);
+        return "hello-list";
     }
 
 }
